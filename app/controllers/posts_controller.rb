ae6b938a838ca80addf7757d0cf78d209d201ae1
FilbertKimasa <filbertdan67@gmail.com>
2024-01-22 12:04:54 +0300

    add posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+  def index
+    # Fetch and display posts with comments for a specific user
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+  end
+
+  def show
+    # Fetch and display a single post with its associated comments
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 end
