1306025f0cdccb10e630f363a012a83e54f75442
FilbertKimasa <filbertdan67@gmail.com>
2024-01-22 12:05:13 +0300

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    # Fetch and display a list of users
+    @users = User.all
+  end
+
+  def posts
+    # Fetch and display posts for a specific user
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
 end
