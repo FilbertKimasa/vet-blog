ca706bde3e919341db201c4fb2b3620221f97039
Filbert Daniel <filbertdan67@gmail.com>
2024-03-13 15:36:52 +0300

    Add like to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts_with_comments'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
+  resources :posts do
+    member do
+      post 'like'
+    end
+  end
 end
